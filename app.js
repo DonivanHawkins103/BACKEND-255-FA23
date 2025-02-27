188b54f2cf623d8f3233e2f1744137f9588ec92e
DonivanHawkins103 <dhawkins103@ivytech.edu>
2025-02-26 21:40:17 -0500

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Song = require("./models/songs");
 var cors = require('cors')
+const jwt = require('jwt-simple')
+const User = require("./models/users")
 
 const app = express();
 app.use(cors())
@@ -9,6 +11,76 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router();
+const secret = "supersecret"
+
+router.post("/user", async(req,res) =>{
+   if(!req.body.username || !req.body.password){
+      res.status(400).json({error: "Missing username or password"})
+   }
+
+   const newUser = await new User({
+      username: req.body.username,
+      password: req.body.password,
+      status: req.body.status
+
+   })
+
+   try{
+      await newUser.save()
+      console.log(newUser)
+      res.sendStatus(201) //created
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+
+})
+
+router.post("/auth", async(req,res) =>{
+   if(!req.body.username || !req.body.password){
+      res.status(400).json({error: "Missing username or password"})
+      return
+   }
+
+   let user = await User.findOne({username : req.body.username})
+      
+   if(!user){
+         res.status(401).json({error : "Bad Username"})
+      }
+      else{
+         if(user.password != rwq.body.password){
+            res.status(401).json({error : "Bad Password"})
+         }
+         else{
+            username2 = user.username
+            const token = jwt.encode({username: user.username}, secret)
+            const auth = 1
+
+            res.json({
+               username2, token: token, auth: auth
+            })
+         }
+      }
+   })
+
+router.get("/status", async function(req, res) {
+
+   // See if the X-Auth header is set
+   if (!req.headers["x-auth"]) {
+      return res.status(401).json(
+         { error: "Missing X-Auth header" });
+   }
+
+   const token = req.headers["x-auth"];
+   try {
+      const decoded = jwt.decode(token, secret);
+
+      let users = User.find({},"username status");
+      res.json(users);
+   }
+   catch (ex) {
+      res.status(401).json({ error: ex.message });}
+});
 
 // Get list of all songs in the database
 router.get("/songs", async(req,res) =>{
@@ -20,7 +92,6 @@ router.get("/songs", async(req,res) =>{
    catch (err){
       console.log(err)
    }
-
 })
 
 //Grab a single song in the database
@@ -44,24 +115,28 @@ router.post("/songs", async(req,res) =>{
    }
    catch(err){
       res.status(400).send(err)
-
    }
-      
-   
 })
 
 //update is to update an existing record/resource/database entry..it uses a put request
 router.put("/songs/:id", async(req,res) =>{
-   //first we need to find and update the song the front end wants us to update.
-   //to do this we need to request the id of the song from request
-   //and the find it in the database and update it
    try{
       const song = req.body
       await Song.updateOne({_id: req.params.id},song)
       console.log(song)
       res.sendStatus(204)
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+})
 
-
+router.delete("/songs/:id", async(req,res) =>{
+   try{
+      const song = await Song.findById(req.params.id)
+      console.log(song)
+      await Song.deleteOne({ _id: song._id })
+      res.sendStatus(204)
    }
    catch(err){
       res.status(400).send(err)
@@ -71,4 +146,7 @@ router.put("/songs/:id", async(req,res) =>{
 
 app.use("/api", router);
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
